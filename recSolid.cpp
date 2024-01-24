89a47763cfb17b05cfd9335478e46f59899dcdb1
Dylan Liesenfelt <dylanjames.code@gmail.com>
2024-01-24 09:37:28 -0500

    updated unit 1

--- recSolid.cpp
+++ recSolid.cpp
@@ -1,3 +1,12 @@
+/*
+    Author: Dylan Liesenfelt
+    Date:  January 21, 2024
+    Program: Rectangular Solid Geometry App
+    File: recSolid.cpp
+    This program prompts a user for length, width, and height of a rectangle and then
+    calculates a volume and a surface area of a rectange and displays results.
+*/
+
 #include <iostream>
 
 using namespace std;
@@ -8,26 +17,26 @@ int main() {
     //declares varriables
     double length, width, height;
 
-    //output that gives user information on the program
-    cout << "This Program can be used to calculate the VOLUME and SURFACE AREA of a rectangular solid.";
+    //Program title output
+    cout << "Rectangular Solid Geometry App ...";
     
     //Gets users input for length width and height vars
-    cout << "ENTER the Length: ";
+    cout << "Please enter rectangle's height: ";
+    cin >> height;
+
+    cout << "                         length: ";
     cin >> length;
 
-    cout << "ENTER the Width: ";
+    cout << "                          width: ";
     cin >> width;
-
-    cout << "ENTER the Height: ";
-    cin >> height;
 
     //Calcs for Volume and Surface Area
     double volume = length * width * height;
     double sa = 2 * (length * height + width * height + width * length);
 
     //Outputs the calculated values
-    cout << "Volume: " << volume << endl;
-    cout << "Surface Area: " << sa << endl;
+    cout << "The volume of the rectangle is " << volume << " units" <<endl;
+    cout << "The surface area of the rectangle is " << sa << "units" << endl;
 
     return 0;
 }
