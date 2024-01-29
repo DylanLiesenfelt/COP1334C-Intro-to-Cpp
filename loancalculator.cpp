0a72d506cdc11cf241f1d2640d805cb7f3484b51
DylanLiesenfelt <dylanjames.code@gmail.com>
2024-01-29 15:23:36 -0500

    Update loancalculator.cpp
    need to fix math

--- loancalculator.cpp
+++ loancalculator.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <cmath>
 
 using namespace std;
 
@@ -16,22 +17,20 @@ using namespace std;
 int main() {
     
     string borrowerName = "", loanInstitution = "", reportMonth = "", reportYear="";
-    double loanAmount = 0, annualIntrestRate = 0, yearsOfTerm = 0, 
-    monthlyIntrestRate = annualIntrestRate / 12, monthsOfTerm = yearsOfTerm * 12, 
-    monthlyPayment = ((monthlyIntrestRate * pow((1 - monthlyIntrestRate), monthsOfTerm)) / (pow((1 + monthlyIntrestRate), monthsOfTerm) - 1)) * loanAmount, 
-    totalAmount = monthlyPayment * monthsOfTerm, totalIntrest = totalAmount - loanAmount;
+    double loanAmount = 0, annualIntrestRate = 0, yearsOfTerm = 0;
+    
 
     cout << "Loan Payment Calculator ..." << endl << endl;
 
     cout << "Please enter the following instructions:" << endl << endl;
 
-    cout << "Borrower's Name: " << endl;
+    cout << "Borrower's Name: ";
     cin >> borrowerName;
 
-    cout << "Loaning Institution:" << endl << endl;
+    cout << endl << "Loaning Institution:";
     cin >> loanInstitution;
 
-    cout << "Loan Amount ($):";
+    cout << endl << endl <<"Loan Amount ($):";
     cin >> loanAmount;
 
     cout << endl << "Annual Intrest Rate (%): ";
@@ -41,24 +40,30 @@ int main() {
     cin >> yearsOfTerm;
 
     cout << endl << endl << "Date of Report: ";
-    cin >> reportMonth >> reportYear
+    cin >> reportMonth >> reportYear;
 
-    cout << endl << endl "-" * 20 << endl;
+    cout << endl << endl << "-----------------------------------------------" << endl;
+    
+    double monthsOfTerm = yearsOfTerm * 12, monthlyIntrestRate = (annualIntrestRate / 100) / 12, 
+    monthlyPayment = ((monthlyIntrestRate * pow((1 - monthlyIntrestRate), monthsOfTerm)) / (pow((1 + monthlyIntrestRate), monthsOfTerm) - 1)) * loanAmount, 
+    totalAmount = monthlyPayment * monthsOfTerm, totalIntrest = totalAmount - loanAmount;
+    
     cout << "Loan Payment Summary Report" << endl;
 
     cout << loanInstitution << " " << borrowerName << endl;
-    cout << "Annual Intrest Rate: " << annualIntrestRate * 100 <<  "%" << endl;
+    cout << "Annual Intrest Rate: " << annualIntrestRate <<  "%" << endl;
     cout << "Date: " << reportMonth << " " << reportYear << endl << endl;
 
-    cout << "Loan Amount: " << "$ " << loanAmount << endl:
-    cout << "Monthly Intrest Rate: " << setprecision(3) << monthlyIntrestRate << "%" << endl;
+    cout << "Loan Amount: " << "$ " << loanAmount << endl;
+    cout << "Monthly Intrest Rate: " << setprecision(2) << monthlyIntrestRate * 100 << "%" << endl;
     cout << "Number of Payments: " << monthsOfTerm << endl;
     cout << "Monthly Payment: " << "$ " << setprecision(3) << monthlyPayment << endl;
 
     cout << "Total Ammount to Pay: " << "$ " << setprecision(3) << totalAmount <<endl;
     cout <<  "Total Interest: " << "$ " << setprecision(3) << totalIntrest << endl << endl;
 
-    cout << endl << endl "-" * 20 << endl;
+    cout << endl << endl << "-----------------------------------------------" << endl;
 
+    /*Need to fix math for monthly payment, total and total interest*/
     return 0;
-}
+};
