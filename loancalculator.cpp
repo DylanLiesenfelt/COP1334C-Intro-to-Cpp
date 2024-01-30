b985ded4a6f881d5827076cd7678c85eb9c10cc6
Dylan Liesenfelt <dylanjames.code@gmail.com>
2024-01-30 16:42:31 -0500

    Update loancalculator.cpp
    math good need to fix output formatting

--- loancalculator.cpp
+++ loancalculator.cpp
@@ -17,7 +17,7 @@ using namespace std;
 int main() {
     
     string borrowerName = "", loanInstitution = "", reportMonth = "", reportYear="";
-    double loanAmount = 0, annualIntrestRate = 0, yearsOfTerm = 0;
+    double loanAmount = 0, annualIntrestRate = 0, yearsOfLoan = 0;
     
 
     cout << "Loan Payment Calculator ..." << endl << endl;
@@ -37,16 +37,16 @@ int main() {
     cin >> annualIntrestRate;
 
     cout <<  endl << "Term of Loan (years): ";
-    cin >> yearsOfTerm;
+    cin >> yearsOfLoan;
 
     cout << endl << endl << "Date of Report: ";
     cin >> reportMonth >> reportYear;
 
     cout << endl << endl << "-----------------------------------------------" << endl;
     
-    double monthsOfTerm = yearsOfTerm * 12, monthlyIntrestRate = (annualIntrestRate / 100) / 12, 
-    monthlyPayment = ((monthlyIntrestRate * pow((1 - monthlyIntrestRate), monthsOfTerm)) / (pow((1 + monthlyIntrestRate), monthsOfTerm) - 1)) * loanAmount, 
-    totalAmount = monthlyPayment * monthsOfTerm, totalIntrest = totalAmount - loanAmount;
+    double monthlyIntrestRate = (annualIntrestRate / 100) / 12, monthsOfLoan = yearsOfLoan * 12, 
+    monthlyPayment = ((monthlyIntrestRate * pow((1 + monthlyIntrestRate), monthsOfLoan))/(pow((1 + monthlyIntrestRate), monthsOfLoan) - 1)) * loanAmount,
+    totalAmount = monthlyPayment * monthsOfLoan, totalIntrest = totalAmount - loanAmount;
     
     cout << "Loan Payment Summary Report" << endl;
 
@@ -56,7 +56,7 @@ int main() {
 
     cout << "Loan Amount: " << "$ " << loanAmount << endl;
     cout << "Monthly Intrest Rate: " << setprecision(2) << monthlyIntrestRate * 100 << "%" << endl;
-    cout << "Number of Payments: " << monthsOfTerm << endl;
+    cout << "Number of Payments: " << monthsOfLoan << endl;
     cout << "Monthly Payment: " << "$ " << setprecision(3) << monthlyPayment << endl;
 
     cout << "Total Ammount to Pay: " << "$ " << setprecision(3) << totalAmount <<endl;
@@ -64,6 +64,6 @@ int main() {
 
     cout << endl << endl << "-----------------------------------------------" << endl;
 
-    /*Need to fix math for monthly payment, total and total interest*/
+    //need to fix formatting
     return 0;
 };
