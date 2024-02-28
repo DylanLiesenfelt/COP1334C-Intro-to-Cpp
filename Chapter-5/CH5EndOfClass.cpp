ad7154ea6876c5a7a4eadbf9271d0edef509a1b4
Dylan Liesenfelt <dylanjames.code@gmail.com>
2024-02-28 11:23:50 -0500

    end of class chapter 5

---
+++ Chapter-5/CH5EndOfClass.cpp
@@ -0,0 +1,44 @@
+/*
+    Author: Dylan Liesenfelt
+    Date:  Febuary 28, 2024
+    
+*/
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+//declares main function of the program
+int main() {
+
+    int students = 0, grade = 0;
+    string name;
+    ofstream outfile;
+
+    outfile.open("grades.txt");
+    
+    if(!outfile){
+        cout << "error";
+    }
+
+    cout << "How Many Students In Class: ";
+    cin >> students;
+    cin.ignore();
+
+    for ( students; students > 0; students--) {
+        cout << "Enter Name: " ;
+        getline(cin, name);
+        cout << endl << "Enter Grade: ";
+        cin >> grade;
+        cin.ignore();
+
+        cout << endl << "Writing " << name << " and grade: " << grade << " to file." << endl << endl;
+        outfile << name << grade;
+    }
+
+    outfile.close();
+
+    return 0;
+}
