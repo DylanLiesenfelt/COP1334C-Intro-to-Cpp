2baa7aa8845498547c4f0f576c5a6d3915ceac67
Dylan Liesenfelt <120532193+DylanLiesenfelt@users.noreply.github.com>
2024-03-24 01:07:00 -0400

    Create unit6lab.cpp

---
+++ Chapter-6/unit6lab.cpp
@@ -0,0 +1,45 @@
+/*
+    Author: Dylan Liesenfelt
+    Date:  , 2024
+    Program: 
+    File: .cpp
+    This program prompts thereturns user for the length of a square and  the width
+*/
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+//declares main function of the program
+int main() {
+    
+    double getPricePerGallon() {
+
+    }
+
+    int getRooms() {
+
+    }
+
+    int calcWallSpace(int rooms) {
+
+    }
+
+    void calcPaint(int sqrFeet, double pricePaint, int& gallons, double& )
+
+    cout << "Paint Job Estimator ..." << endl << endl;
+    cout << "Price per gallon of paint (>-0): ";
+    cin >> pricePaint;
+
+    cout << endl << endl << "Number of rooms to be painted (>=1): ";
+    cin >> rooms;
+
+    cout << endl << endl << "Paint Job Estimate" << endl << endl;
+    cout << "Paint ...";
+
+    cout << "Labor";
+
+    return 0;
+}
