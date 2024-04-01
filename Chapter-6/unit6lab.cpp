5c0ac447cc45ae133a25afa29e429f6fe553f922
DylanLiesenfelt <dylanjames.code@gmail.com>
2024-03-31 22:09:44 -0400

    Update unit6lab.cpp

--- Chapter-6/unit6lab.cpp
+++ Chapter-6/unit6lab.cpp
@@ -15,31 +15,40 @@ using namespace std;
 //declares main function of the program
 int main() {
     
-    double getPricePerGallon() {
-
-    }
-
-    int getRooms() {
-
-    }
-
-    int calcWallSpace(int rooms) {
-
-    }
-
-    void calcPaint(int sqrFeet, double pricePaint, int& gallons, double& )
+    double getPricePerGallon();
+    int getRooms();
+    int calcWallSpace(int rooms);
+    void calcPaint(int sqrFeet, double pricePaint, int& gallons, double& cost);
+    void calcLabor(int sqrFeet, double& hours, double& cost);
 
     cout << "Paint Job Estimator ..." << endl << endl;
-    cout << "Price per gallon of paint (>-0): ";
-    cin >> pricePaint;
+    
+    cout << "Price per gallon of paint (>=0): ";
+    getline(cin, pricePaint);
+    if (pricePaint < 0) {
+        cout << "Error ... Invalid price per gallon of paint. Try Agian." << endl << endl;
+        cout << "Price per gallon of paint (>=0): ";
+        getline(cin, pricePaint);
+    } 
 
-    cout << endl << endl << "Number of rooms to be painted (>=1): ";
-    cin >> rooms;
+    cout << endl << endl << "Number of rooms to be painted (>=1): " ;
+    getline(cin, rooms);
+    if (rooms < 1) {
+        cout << "Error ... Invalid number of rooms. Try Again.";
+        cout << endl << endl << "Number of rooms to be painted (>=1): " ;
+        getline(cin, rooms);
+    }
 
-    cout << endl << endl << "Paint Job Estimate" << endl << endl;
-    cout << "Paint ...";
-
-    cout << "Labor";
+    cout << "Square feet of wall space (>=25)" << endl;
+    int count = 0;
+    while (count != rooms) {
+        cout << "Room " << count << ": ";
+        cin >> temp;
+        sqrFeet += temp;
+        cin.ignore();
+        count += 1;
+    }
+    
 
     return 0;
 }
