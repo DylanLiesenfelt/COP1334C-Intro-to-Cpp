068755a55199725119a50ee50ea85370d2490f71
Dylan Liesenfelt <dylanjames.code@gmail.com>
2024-04-10 15:34:33 -0400

    end of class 7

---
+++ Chapter 7/endOfClass7.cpp
@@ -0,0 +1,33 @@
+/*
+    Author: Dylan Liesenfelt
+    Date:  April 10th, 2024
+*/
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int students = 0;
+
+
+    cout << "Enter the number of Students: ";
+    cin >> students;
+    cin.ignore();
+
+    for (int i = 0; i < students; i++) {
+        cout << "Enter Student Name: ";
+        cin.getline(name);
+        
+        cout >> "Enter Student Age: ";
+        cin << age;
+
+        l
+    }
+
+
+
+    return 0;
+}
