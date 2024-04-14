7d8ee787e735d5aa4b9067f8559164298862f302
Dylan Liesenfelt <dylanjames.code@gmail.com>
2024-04-14 16:13:01 -0400

    updated
    added 2d array, checks good needs comments

--- Chapter 7/loShu.cpp
+++ Chapter 7/loShu.cpp
@@ -11,123 +11,97 @@
 
 using namespace std;
 
-int inputNumber = 0;
-int numberArray[8] = {};
-bool inArray = false;
+const int ROWS = 3;
+const int COLS = 3;
 
-bool arrayCheck(int num, int arr[], int size) {
-    for (int i = 0; i < size; i++){
-        if (num == arr[i]) {
-            return true;
+bool arrayCheck(int num, int arr[ROWS][COLS], int currentRow, int currentCol) {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            if (i != currentRow || j != currentCol) { 
+                if (num == arr[i][j]) {
+                    return true;
+                }
+            }
         }
     }
     return false;
 }
 
-void makeSquare(int arr[]) {
-    string line = "-------------" ;
-    int count = 0;
+
+void makeSquare(int arr[ROWS][COLS]) {
+    string line = "-------------";
     int r1 = 0, r2 = 0, r3 = 0, c1 = 0, c2 = 0, c3 = 0, d1 = 0, d2 = 0;
 
     cout << line << endl;
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++) {
-            cout << "|" << " " << arr[count] << " ";
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            cout << "|" << " " << arr[i][j] << " ";
 
-            while (count == 0 || count == 3 || count == 6) {
-                c1 += arr[count];
-                break;
-            }
-
-            while (count == 1 || count == 4 || count == 7) {
-                c2 += arr[count];            break;
+            if (i == 0) {
+                c1 += arr[i][j];
+            } else if (i == 1) {
+                c2 += arr[i][j];
+            } else if (i == 2) {
+                c3 += arr[i][j];
             }
 
-            while (count == 2 || count == 5 || count == 8) {
-                c3 += arr[count];
-                break;
+            if (j == 0) {
+                r1 += arr[i][j];
+            } else if (j == 1) {
+                r2 += arr[i][j];
+            } else if (j == 2) {
+                r3 += arr[i][j];
             }
 
-            while (count == 0 || count == 1 || count == 2) {
-                r1 += arr[count];
-                break;
+            if (i == j) {
+                d1 += arr[i][j];
             }
 
-            while (count == 3 || count == 4 || count == 5) {
-                r2 += arr[count];
-                break;
+            if (i + j == ROWS - 1) {
+                d2 += arr[i][j];
             }
-
-            while (count == 6 || count == 7 || count == 8) {
-                r3 += arr[count];
-                break;
-            }
-
-            while (count == 0 || count == 4 || count == 8) {
-                d1 += arr[count];
-                break;
-            }
-
-            while (count == 2 || count == 4 || count == 6) {
-                d2 += arr[count];
-                break;
-            }
-            
-            while (count == 2 || count == 5 || count == 8) {
-                cout << "|";
-                break;
-            }
-            
-            count++;
         }
-        cout << endl << line << endl; 
-    }
-    
-    if (r1 == 15 && r2 == 15 && r3 == 15 && c1 == 15 && c2 == 15 && c3 == 15) {
-        cout << endl << "This is a Lo Sho Square!!!" << endl << endl;
+        cout << "|" << endl << line << endl;
     }
 
-    else {
+    if (r1 == 15 && r2 == 15 && r3 == 15 && c1 == 15 && c2 == 15 && c3 == 15 && d1 == 15 && d2 == 15) {
+        cout << endl << "This is a Lo Sho Square!!!" << endl << endl;
+    } else {
         cout << endl << "Sorry ... this is not a Lo Sho Square" << endl << endl;
     }
-} 
+}
 
-
-//declares main function of the program
 int main() {
-
     char playAgain = 'y';
 
-    while (playAgain == 'y' || playAgain == 'Y'){
+    while (playAgain == 'y' || playAgain == 'Y') {
+
         cout << "Creating Lo Shu Square ..." << endl << endl;
 
+        int numberArray[ROWS][COLS] = {0}; 
+
         cout << "Enter Nine Numbers (1-9)" << endl;
-        for (int i = 0; i < 9; i++){
-            cout << setw(15) << right << "Number " << (i+1) << ": ";
-            cin >> inputNumber;
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                cout << setw(15) << right << "Number " << (i * COLS + j + 1) << ": ";
+                cin >> numberArray[i][j];
 
-            while (inputNumber < 1 || inputNumber > 9) {
-                cout << setw(43) << right << "Error ... Invalid number. Try again" << endl << endl;
-                cin.ignore();
-                cout << setw(15) << right << "Number " << (i+1) << ": ";
-                cin >> inputNumber;
+                while (numberArray[i][j] < 1 || numberArray[i][j] > 9) {
+                    cout << setw(43) << right << "Error ... Invalid number. Try again" << endl << endl;
+                    cin.ignore();
+                    cout << setw(15) << right << "Number " << (i * COLS + j + 1) << ": ";
+                    cin >> numberArray[i][j];
+                }
+
+                while (arrayCheck(numberArray[i][j], numberArray, i, j)) {
+                    cout << setw(18) << right << "Error ... " << numberArray[i][j] << " is already in the Lo Shu Square. Try again" << endl << endl;
+                    cin.ignore();
+                    cout << setw(15) << right << "Number " << (i * COLS + j + 1) << ": ";
+                    cin >> numberArray[i][j];
+                }
             }
+        }
 
-            inArray = arrayCheck(inputNumber, numberArray, i);
-            while (inArray == true) {
-                cout << setw(18) << right << "Error ... " << inputNumber << " is already in the Lo Shu Square. Try Again" << endl << endl;
-                cin.ignore();
-                cout << setw(15) << right << "Number " << (i+1) << ": ";
-                cin >> inputNumber;
-                inArray = arrayCheck(inputNumber, numberArray, i);
-            }
-        
-            numberArray[i] = inputNumber;
-            cin.ignore();
-        }   
-
-        //OUTPUTS
         cout << endl;
         makeSquare(numberArray);
 
