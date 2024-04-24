4bb4c6dc79eb9837e4dd789109c8df4e58f28d05
Dylan Liesenfelt <dylanjames.code@gmail.com>
2024-04-24 12:31:29 -0400

    Update endOfClassUnit8.cpp

--- Chapter-8/endOfClassUnit8.cpp
+++ Chapter-8/endOfClassUnit8.cpp
@@ -23,13 +23,26 @@ int main() {
         int year;
     };
 
-    vehicleList[];
+    
 
     cout << "How many vehicles do you have";
     cin << numberOfVehicles;
+
+    Vehicle cars[numberOfVehicles];
     
-    for (int i = 0; i < vehicleList[]; i++ ) {
-        
+    for (int i = 0; i < cars[numberOfVehicles]; i++ ) {
+        cout << endl << "What is the brand of the vehicle?";
+        getline(cin, cars[i].brand);
+        cout << endl << "What is the model of the vehicle?";
+        getline(cin, cars[i].make);
+        cout << endl << "What year is the year of the vehicle?";
+        getline(cin, cars[i].year)
+    }
+
+    cout << endl << endl << "Inventory consist of the following vehicles.";
+    cout << setw(5) << right << "Brand" << setw(5) << right << "Make" << setw(5) << right << "Year";
+    for (int j = 0; i < cars[numberOfVehicles]; i++ ) {
+        cout << setw(5) << right << cars[i].brand << setw(5) << right << cars.setw(5) <<  
     }
 
     
