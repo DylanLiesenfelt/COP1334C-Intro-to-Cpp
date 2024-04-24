55c9876f6863758a0b2620390730029ccbbaa1ac
Dylan Liesenfelt <120532193+DylanLiesenfelt@users.noreply.github.com>
2024-04-24 11:09:22 -0400

    endofclass8 int

---
+++ Chapter-8/endOfClassUnit8.cpp
@@ -0,0 +1,39 @@
+/*
+    Author: Dylan Liesenfelt
+    Date:  , 2024
+    Program: 
+    File: .cpp
+    This program prompts thereturns user for the length of a square and  the width
+*/
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+//declares main function of the program
+int main() {
+
+    int numberOfVehicles;
+
+    struct Vehicle{
+        string brand;
+        string model;
+        int year;
+    };
+
+    vehicleList[];
+
+    cout << "How many vehicles do you have";
+    cin << numberOfVehicles;
+    
+    for (int i = 0; i < vehicleList[]; i++ ) {
+        
+    }
+
+    
+
+
+    return 0;
+}
