ed2346d97b3abb1fbfc3f03edbb20f1396da1bf4
DylanLiesenfelt <dylanjames.code@gmail.com>
2024-04-24 20:21:20 -0400

    Update endOfClassUnit8.cpp

--- Chapter-8/endOfClassUnit8.cpp
+++ Chapter-8/endOfClassUnit8.cpp
@@ -10,22 +10,22 @@ using namespace std;
 
 //declares main function of the program
 int main() {
+    
+    int numberOfVehicles; // Counter
 
-    int numberOfVehicles;
-
-    struct Vehicle{
+    struct Vehicle{ // Struct for vehicle type
         string brand;
         string model;
         int year;
     };
 
-    cout << "How many vehicles do you have? ";
+    cout << "How many vehicles do you have? "; // Input for counter
     cin >> numberOfVehicles;
     cin.ignore();
 
-    Vehicle cars[numberOfVehicles];
+    Vehicle cars[numberOfVehicles]; // Array using Vehicle type to store user inputs 
 
-    for (int i = 0; i < numberOfVehicles; i++ ) {
+    for (int i = 0; i < numberOfVehicles; i++ ) { // Itterates to take in data from user
         cout << endl << "What is the brand of the vehicle? ";
         getline(cin, cars[i].brand);
         cout << "What is the model of the vehicle? ";
@@ -37,7 +37,7 @@ int main() {
 
     cout << endl << endl << "Inventory consist of the following vehicles." << endl;
     cout << setw(15) << right << "Brand" << setw(15) << right << "Make" << setw(15) << right << "Year" << endl;
-    for (int j = 0; j < numberOfVehicles; j++ ) {
+    for (int j = 0; j < numberOfVehicles; j++ ) { //Itterates to display data taken from user
         cout << setw(15) << right << cars[j].brand << setw(15) << right << cars[j].model << setw(15) << right << cars[j].year << endl;  
     }
 
