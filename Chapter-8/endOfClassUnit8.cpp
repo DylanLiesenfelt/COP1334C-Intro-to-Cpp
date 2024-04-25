9b942904e361a0f5050139eac032be42545dfbdc
DylanLiesenfelt <dylanjames.code@gmail.com>
2024-04-24 20:18:32 -0400

    update

--- Chapter-8/endOfClassUnit8.cpp
+++ Chapter-8/endOfClassUnit8.cpp
@@ -1,14 +1,10 @@
 /*
     Author: Dylan Liesenfelt
-    Date:  , 2024
-    Program: 
-    File: .cpp
-    This program prompts thereturns user for the length of a square and  the width
+    Date:  April 24, 2024
 */
 #include <iostream>
 #include <string>
 #include <iomanip>
-#include <cmath>
 
 using namespace std;
 
@@ -23,30 +19,27 @@ int main() {
         int year;
     };
 
-    
-
-    cout << "How many vehicles do you have";
-    cin << numberOfVehicles;
+    cout << "How many vehicles do you have? ";
+    cin >> numberOfVehicles;
+    cin.ignore();
 
     Vehicle cars[numberOfVehicles];
-    
-    for (int i = 0; i < cars[numberOfVehicles]; i++ ) {
-        cout << endl << "What is the brand of the vehicle?";
+
+    for (int i = 0; i < numberOfVehicles; i++ ) {
+        cout << endl << "What is the brand of the vehicle? ";
         getline(cin, cars[i].brand);
-        cout << endl << "What is the model of the vehicle?";
-        getline(cin, cars[i].make);
-        cout << endl << "What year is the year of the vehicle?";
-        getline(cin, cars[i].year)
+        cout << "What is the model of the vehicle? ";
+        getline(cin, cars[i].model);
+        cout << "What year is the year of the vehicle? ";
+        cin >> cars[i].year;
+        cin.ignore();
     }
 
-    cout << endl << endl << "Inventory consist of the following vehicles.";
-    cout << setw(5) << right << "Brand" << setw(5) << right << "Make" << setw(5) << right << "Year";
-    for (int j = 0; i < cars[numberOfVehicles]; i++ ) {
-        cout << setw(5) << right << cars[i].brand << setw(5) << right << cars.setw(5) <<  
+    cout << endl << endl << "Inventory consist of the following vehicles." << endl;
+    cout << setw(15) << right << "Brand" << setw(15) << right << "Make" << setw(15) << right << "Year" << endl;
+    for (int j = 0; j < numberOfVehicles; j++ ) {
+        cout << setw(15) << right << cars[j].brand << setw(15) << right << cars[j].model << setw(15) << right << cars[j].year << endl;  
     }
-
-    
-
 
     return 0;
 }
