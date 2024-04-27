7b073121ca4e910c80d7aaaf7325e444dea594b9
DylanLiesenfelt <dylanjames.code@gmail.com>
2024-04-27 01:39:36 -0400

    lab8 done, needs comments

--- Chapter-8/lab8.cpp
+++ Chapter-8/lab8.cpp
@@ -15,9 +15,11 @@ using namespace std;
 //declares main function of the program
 int main() {
     
-    int menu = 0, totalGoals = 0;
-
-    string inputFileName;
+    int menu = 0, totalGoals = 0, playerNumber = 0, playerGoals = 0, highestGoal = 0, fileCount = 0; 
+    string inputFileName, firstName, lastName, topPlayer;
+    ifstream infile;
+    bool run = true;
+            
 
     struct Player {
         string name;
@@ -25,49 +27,83 @@ int main() {
         int goal;
     };
 
-    cout << "Soccer Team Goals ..." << endl << endl;
+    Player teamArray[11];
 
-    cout << "Choose one of the following options" << endl;
-    cout << setw(15) << right << "1. Load team's information." << endl;
-    cout << setw(15) << right << "2. Display team's roster." << endl;
-    cout << setw(15) << right << "3. Display team's goals." << endl;
-    cout << setw(15) << right << "4. Display tema's star(s)." << endl;
-    cout << setw(15) << right << "Quit" << endl;
-    cout << "Option: ";
-    getline(cin, menu);
+    cout << "Soccer Team Goals ..." << endl;
 
-    switch (menu) {
-        case 1:
-            cout << "Enter File Name: ";
-            getline(cin, inputFileName);
-        break;
+   while (run = true) {
 
-        case 2:
-            cout << endl << "Player Name" << setw(20) << left << "Number" << setw(10) << left << "Goals" << endl;
-            cout << "-----------------------------------------" << endl;
+        cout << endl << "Choose one of the following options" << endl;
+        cout << left << setw(8) << " " << "1. Load team's information." << endl;
+        cout << left << setw(8) << " " << "2. Display team's roster." << endl;
+        cout << left << setw(8) << " " << "3. Display team's goals." << endl;
+        cout << left << setw(8) << " " << "4. Display team's star(s)." << endl;
+        cout << left << setw(8) << " " << "5. Quit" << endl;
+        cout << "Option: ";
+        cin >> menu;
+        cin.ignore();
 
-        break;
+        switch (menu) {
+            case 1:
+                cout << endl << "Enter File Name: ";
+                getline(cin, inputFileName);
+                infile.open(inputFileName);
 
-        case 3:
-            cout << "Total goals scored: " << totalGoals << endl;
-        break;
+                if (!infile) { //Input validation, if user input file does not exist displays error, breaks out of switch statment returns user to main menu.
+                        cout << endl << "Error ... Cannot open " << inputFileName << endl;
+                        break;
+                };
+                
+                while (infile >> firstName >> lastName >> playerNumber >> playerGoals) {
+                    teamArray[fileCount].name = firstName + " " + lastName;
+                    teamArray[fileCount].number = playerNumber;
+                    teamArray[fileCount].goal = playerGoals;
+                    totalGoals += playerGoals;
+                    
+                    if (teamArray[fileCount].goal > highestGoal) {
+                        topPlayer = teamArray[fileCount].name;
+                        highestGoal =  teamArray[fileCount].goal;
+                    };
 
-        case 4:
-            cout << "Top team player(s)"
-        break;
+                    fileCount++; 
 
-        case 5:
-        break;
+                };
 
-        default:
-        cout << "ERROR."
-        break;
+                infile.close();
+
+            break;
+
+            case 2:
+                cout << endl << left << setw(20) << "Player Name" << left << setw(10) << "Number" << left << setw(10) << "Goals" << endl;
+                cout << "-----------------------------------------" << endl;
+                for (int i = 0; i < 11; i++) {
+                    cout << left << setw(20) << teamArray[i].name << left << setw(10) << teamArray[i].number << left << setw(10) << teamArray[i].goal << endl;
+                };
+            break;
+
+            case 3:
+                cout << endl << "Total goals scored: " << totalGoals << endl;
+            break;
+
+            case 4:
+                cout << endl << "Top team player(s): " << topPlayer << "  ( " << highestGoal << " goals )" << endl;
+            break;
+
+            case 5:
+            break;
+
+            default:
+            cout << endl << "ERROR." << endl;
+            break;
+        }
+
+        if (menu == 5) {
+            cout << endl << "Goodbye ... ";
+            run = false;
+            break;
+        }
     }
-
-    if (menu == 5) {
-        cout << "Goodbye ... ";
-        break;
-    }
+   
     
 
     return 0;
